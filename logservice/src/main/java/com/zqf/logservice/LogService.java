c8d50fdbbb35bdc8b2d147ede876e12249bdc4e4
zhangqufa <afa1989@126.com>
2020-12-03 16:59:25 +0800

    适合android 8.0 ,在onStartCommand()方法中执笔 startForeground .v3 release

--- logservice/src/main/java/com/zqf/logservice/LogService.java
+++ logservice/src/main/java/com/zqf/logservice/LogService.java
@@ -81,7 +81,6 @@ public class LogService extends Service {
         register();
         deploySwitchLogFileTask();
         logCollector();
-        startForeNotice();
     }
 
     private void startForeNotice() {
@@ -107,6 +106,7 @@ public class LogService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        startForeNotice();
         return Service.START_STICKY;
     }
 
